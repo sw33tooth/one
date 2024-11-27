78a1a32d69ffb6a7008108ff4011e464d0f9534d
nicolas <144283446+sw33tooth@users.noreply.github.com>
2024-11-26 23:23:51 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Ejemplo: Alerta al pulsar botones
+
+document.querySelector('.review-button').addEventListener('click', () => {
+    alert('Abrir sección de reseñas.');
+  });
+  
+  document.querySelector('.chat-button').addEventListener('click', () => {
+    alert('Abrir chat de soporte.');
+  });
+  
